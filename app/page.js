8297237172eb1a5f38199ab6a38531d9b862bfba
Vinitha Rajagopal <113119547+vini-raj97@users.noreply.github.com>
2023-12-15 16:45:57 -0700

    Updated page.js

--- app/page.js
+++ app/page.js
@@ -10,16 +10,18 @@ export default function Home() {
       </header>
 
       <main className="flex min-h-screen flex-col font-serif items-center">
-        <div
-          className="hero-image items-center p-7 flex mb-5 shadow-2xl"
-          style={{ backgroundImage: "url('/images/home-page-background.jpg')" }}
-        >
-          <HeroBanner
-            bclass={"text-white bg-black p-2 rounded-md text-slate-600 text-xs"}
-            title={"Build strength with this comprehensive training program"}
-            thislink={"/products"}
-            linkname={"view contents"}
-          />
+        <div className="container max-w-7xl mx-auto px-4">
+          <div
+            className="hero-image items-center p-7 flex mb-5 shadow-2xl"
+            style={{ backgroundImage: "url('/images/home-page-background.jpg')" }}
+          >
+            <HeroBanner
+              bclass={"text-white bg-black p-2 rounded-md text-slate-600 text-xs"}
+              title={"Build strength with this comprehensive training program"}
+              thislink={"/products"}
+              linkname={"view contents"}
+            />
+          </div>
         </div>
       </main>
     </>
